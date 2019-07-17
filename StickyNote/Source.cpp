7d9200802ca9998546ec6d49c2c213a7cedac554
Samar Jamjom <samarjamjom@gmail.com>
2019-07-17 13:51:08 +0300

    Interface II is ready

--- StickyNote/Source.cpp
+++ StickyNote/Source.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
 
 int main()
 {
+	bool work ; // to find out if user need to return to main menu
 
+	do
+	{
+	work = false;
 cout << "Welcome to the brand new "<<"Sticky Notes! \n"
 	 << "Here is the list of operation this program offers:\n\n"
 	 <<"1- Add new user\n"
@@ -20,9 +26,25 @@ switch (choice )
 {
 
 case 1:
+	{
+	cout << "Welcome aboard new user!\n"
+	     << "Please let me know your first name: \n";
+		string FirstName;
+		cin >> FirstName;
+		cout << "now please enter your last name: \n";
+		string LastName;
+		cin >> LastName;
+		cout << "Nice to meet you "<< FirstName << " " <<LastName << endl;
 
+		ofstream fout;
+		fout.open( FirstName + LastName + ".txt");
 
+		cout << "Please Click Enter to return to main menu \n \n";
+		cin.get();
+		if (cin.get() == '\n')
+			work = true;
 
+	}
 	break;
 
 case 2:
@@ -49,6 +71,8 @@ case 4:
 	break;
 }
 	
+	}while(work);
+
 system ("pause");
 return 0;
 }
