0344de393a2db5532175fd86698eedc4afe4991f
Samar Jamjom <samarjamjom@gmail.com>
2019-07-17 12:37:14 +0300

    InterfaceShape

--- StickyNote/Source.cpp
+++ StickyNote/Source.cpp
@@ -1,14 +1,54 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
 
+cout << "Welcome to the brand new "<<"Sticky Notes! \n"
+	 << "Here is the list of operation this program offers:\n\n"
+	 <<"1- Add new user\n"
+	 <<"2- Add new note\n"
+	 <<"3- View notes for a specific user\n"
+	 <<"4- Exit\n\n";
+
+cout << "Please Enter Your Choice \n";
+int choice;
+cin >> choice ;
+
+switch (choice )
+{
+
+case 1:
+
+
+
+	break;
+
+case 2:
 
 
 
 
+	break;
 
 
+
+case 3:
+
+
+
+	break;
+
+
+
+case 4:
+
+
+
+	break;
+}
+	
+system ("pause");
 return 0;
 }
